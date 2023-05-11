a18e55e2ef27811bc0ba916c897a865c822326b5
Elvin Agha <33497735+L-win@users.noreply.github.com>
2023-05-11 12:01:59 +0400

    Simple request to WeatherApi.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,34 @@
 package org.example;
 
 import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class Main {
 
+	private static final String API_KEY = "";
+
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Start Test.");
 	}
 
 	@Test(priority=1)
-	public static void testOne(){ }
+	private static void testOne(){
+		String q = "";
+		String request = "http://api.weatherapi.com/v1/q="+q+"/key"+API_KEY;
+		request = "http://api.weatherapi.com/v1/q=48.8567,2.3508/current.json";
+		request = "http://api.weatherapi.com/v1/current.json?key="+API_KEY+"&q=48.8567,2.3508";
+
+		given()
+				.when()
+					.get(request)
+				.then()
+				.statusCode(200);
+	}
+
+//	@Test(priority=2)
+	private static void testTwo(){}
+
 
 }
