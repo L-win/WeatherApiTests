a19a259c163975bec210dda218c12405f3327bab
Elvin Agha <33497735+L-win@users.noreply.github.com>
2023-05-11 12:18:36 +0400

    Store Api key in file.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,31 @@
 package org.example;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class Main {
 
-	private static final String API_KEY = "";
-
+	@BeforeClass
 	public static void main(String[] args) {
 		System.out.println("Start Test.");
 	}
 
 	@Test(priority=1)
 	private static void testOne(){
-		String q = "";
-		String request = "http://api.weatherapi.com/v1/q="+q+"/key"+API_KEY;
-		request = "http://api.weatherapi.com/v1/q=48.8567,2.3508/current.json";
-		request = "http://api.weatherapi.com/v1/current.json?key="+API_KEY+"&q=48.8567,2.3508";
+		String apiKey = getApiKey();
+		String request = "http://api.weatherapi.com/v1/current.json?key="+apiKey+"&q=48.8567,2.3508";
+
+		String host = "http://api.weatherapi.com/v1/";
+		String q = "current.json?key=\"+apiKey+\"&q=48.8567,2.3508\"";
 
 		given()
 				.when()
@@ -30,5 +37,21 @@ public class Main {
 //	@Test(priority=2)
 	private static void testTwo(){}
 
+	private static String getApiKey(){
+		String result = "";
+		try {
+			File myObj = new File("APIKEY");
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				result = data;
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		return result;
+	}
 
 }
