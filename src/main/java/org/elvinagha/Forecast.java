b68e33ac1339d1dc389122433061f63cf6863bf6
Elvin Agha <33497735+L-win@users.noreply.github.com>
2023-05-13 12:33:10 +0400

    Make a request for Forecast data.

--- src/main/java/org/elvinagha/Forecast.java
+++ src/main/java/org/elvinagha/Forecast.java
@@ -2,24 +2,38 @@ package org.elvinagha;
 
 import io.restassured.response.Response;
 import org.elvinagha.util.ApiKey;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class Forecast {
 
-	@Test(priority=7)
-	private static void ForeCast(){
+	@DataProvider(name="request")
+	private static Object[] getRequest(){
 		String host = "http://api.weatherapi.com/v1/";
 		String category = "forecast.json";
 		String apiKey = "?key=" + ApiKey.getApiKey();
 		String q = "&q=London";
 		String request = host + category + apiKey + q;
 
-		Response res = given().when().get(request)
-				.then().statusCode(200)
-				.log().body().extract().response();
+		Response res =
+				given()
+						.when().get(request)
+				.then()
+						.statusCode(200)
+				.extract()
+						.response();
 
-		String response = res.asString();
+		// System.out.println(res.asString());
+		return new Object[]{res.asString()};
 	}
+
+	@Test(priority=1,dataProvider="request")
+	private static void TEMP(String request){
+		System.out.println(request);
+	}
+
+	@Test(priority=1,dataProvider="request")
+	private static void t1_City(String request){}
 }
